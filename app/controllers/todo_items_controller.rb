6bca85bdc55537b3cdcd065b9de476da7be244f7
veepgoose <victoriaplatt@loveplanes.co.uk>
2023-09-12 09:51:27 +0100

    Still debugging! - Transferring to a different machine to continue

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -4,7 +4,20 @@ class TodoItemsController < ApplicationController
     @todo_item = @todo_list.todo_items.create(todo_item_params)
     redirect_to @todo_list
    end
-   private
+
+   def destroy
+    @todo_item = TodoItem.find(params[:id])
+    if @todo_item.destroy
+      flash[:success] = "Todo List item was deleted."
+    else
+      flash[:error] = "Todo List item could not be deleted."
+    end
+    redirect_to todo_list_path(@todo_list)
+  end
+  
+  
+   
+    private
    def set_todo_list
     @todo_list = TodoList.find(params[:todo_list_id])
    end
